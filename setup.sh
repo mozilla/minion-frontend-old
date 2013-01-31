a9af49f7e7c130820056f9ca4977d59161e5211a
Stefan Arentz <stefan@arentz.ca>
2013-01-30 19:05:23 -0500

    Added a setup script for development

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -x
+
+# This script expects an active virtualenv
+
+if [ -z "$VIRTUAL_ENV" ]; then
+    echo "abort: no virtual environment active"
+    exit 1
+fi
+
+case $1 in
+    develop)
+        pip install -r requirements/compiled.txt || exit 1
+        ;;
+esac
